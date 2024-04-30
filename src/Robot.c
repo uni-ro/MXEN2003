7cf026f292df8bf97852d23afff4fa2d90ceb9f2
Thomas <21547533@student.curtin.edu.au>
2024-04-30 17:04:22 +0800

    Lab9

--- src/Robot.c
+++ src/Robot.c
@@ -15,22 +15,33 @@ volatile bool new_message_received_flag=false;
 int main(void)
 {
   
-  DDRA = 1;
-  DDRB |= (1 << PB6);
+	DDRA = 1;
+	DDRB |= (1 << PB6);
 
-  // Init for TCCR1
-  TCCR1A |= (1<<WGM10); // Set to mode 9
-  TCCR1B |= (1<<WGM13);
+	// Init for TCCR1
+	TCCR1A |= (1<<WGM10); // Set to mode 9
+	TCCR1B |= (1<<WGM13);
 
-  TCCR1A |= (1<<COM1B0 | 1<<COM1B1); // Set to clear on down and set on up
-  TCCR1B |= (1<<CS10);
+	TCCR1A |= (1<<COM1B0 | 1<<COM1B1); // Set to clear on down and set on up
+	TCCR1B |= (1<<CS10);
 
-  OCR1B = 10000; // The COMP value for 50% speed
-  OCR1A = 20000; // The TOP value for 400Hz
+	OCR1B = 10000; // The COMP value for 50% speed
+	OCR1A = 20000; // The TOP value for 400Hz
 
-  // initialisation
+	// Init for TCCR3
+	TCCR3A |= (1<<WGM30); // Set to mode 9
+	TCCR3B |= (1<<WGM33);
+
+	TCCR3A |= (1<<COM3B0 | 1<<COM3B1); // Set to clear on down and set on up
+	TCCR3B |= (1<<CS30);
+
+	OCR3B = 10000; // The COMP value for 50% speed
+	OCR3A = 20000; // The TOP value for 400Hz
+
+	// initialisation
 	serial0_init(); 	// terminal communication with PC
 	serial2_init();		// microcontroller communication to/from another Arduino
+	adc_init();			// Initialise ADC
 	// or loopback communication to same Arduino
 	
 	UCSR2B |= (1 << RXCIE2); // Enable the USART Receive Complete interrupt (USART_RXC)
@@ -38,62 +49,71 @@ int main(void)
 	milliseconds_init();
 	sei();
 
-  while(1)//main loop
-  {
+	uint16_t batteryVoltage;
 
-    //if a new byte has been received
+	while(1)//main loop
+	{
+
+		batteryVoltage = adc_read(0);
+
+		if (batteryVoltage <= 717) // If battery voltage is below 7V (Input Voltage = 3.5V), turn LED On
+		{
+			PORTA |= (1 << PA4);
+		}
+		else
+		{
+			PORTA &= ~(1 << PA4); // Else turn LED Off
+		}
+
+		//if a new byte has been received
 		if(new_message_received_flag) 
 		{
 
-      uint32_t leftStickH = (((uint16_t) dataByte1) << 8) + ((uint16_t) dataByte2);
-      uint32_t rightStick = (((uint16_t) dataByte3) << 8) + ((uint16_t) dataByte4);
-      uint32_t leftStickV = (((uint16_t) dataByte5) << 8) + ((uint16_t) dataByte6);
+			uint32_t leftStickH = (((uint16_t) dataByte1) << 8) + ((uint16_t) dataByte2);
+			uint32_t rightStick = (((uint16_t) dataByte3) << 8) + ((uint16_t) dataByte4);
+			uint32_t leftStickV = (((uint16_t) dataByte5) << 8) + ((uint16_t) dataByte6);
 
-			// now that a full message has been received, we can process the whole message
-			// the code in this section will implement the result of your message
-			//sprintf(serial_string, "received: 1:%4d, 2:%4d , 3:%4d , 4:%4d \n Total: L: %4u\t R: %4u\n", dataByte1, dataByte2, dataByte3, dataByte4, newByte, newByte2);
-			//serial0_print_string(serial_string);  // print the received bytes to the USB serial to make sure the right messages are received
+					// now that a full message has been received, we can process the whole message
+					// the code in this section will implement the result of your message
+					//sprintf(serial_string, "received: 1:%4d, 2:%4d , 3:%4d , 4:%4d \n Total: L: %4u\t R: %4u\n", dataByte1, dataByte2, dataByte3, dataByte4, newByte, newByte2);
+					//serial0_print_string(serial_string);  // print the received bytes to the USB serial to make sure the right messages are received
 
-      OCR1B = rightStick * 20000 / 1021;
+			OCR1B = rightStick * 20000 / 1021;
+			OCR3B = rightStick * 20000 / 1021;
 
-      sprintf(serial_string, "received: Left Stick Horizontal: %4lu, Left Stick Vertical: %4lu, Right Stick: %lu\n", leftStickH, leftStickV, rightStick);
-      serial0_print_string(serial_string);
+			sprintf(serial_string, "received: Left Stick Horizontal: %4lu, Left Stick Vertical: %4lu, Right Stick: %lu\n", leftStickH, leftStickV, rightStick);
+			serial0_print_string(serial_string);
 
-			new_message_received_flag=false;	// set the flag back to false
-      
-      if(leftStickV >= 600)//move forward
-      {
-        PORTA |= ((1<< PA0) | (1<<PA3));
-        PORTA &= ~((1<< PA1) | (1<<PA2));
-      }
-      else if(leftStickV <= 400) //move back
-      {
-        PORTA |= ((1<< PA1) | (1<<PA2));
-        PORTA &= ~((1<< PA0) | (1<<PA3));
-      }
-      else if(leftStickH >= 600) // move right
-      {
-        PORTA |= ((1<< PA0) | (1<<PA2));
-        PORTA &= ~((1<< PA1) | (1<<PA3));
-      }
-      else if(leftStickH <= 400)//move left
-      {
-        PORTA |= ((1<< PA1) | (1<<PA3));
-        PORTA &= ~((1<< PA0) | (1<<PA2));
-      }
-      else 
-      {
-        PORTA &= ~((1<< PA1) | (1<<PA3));
-        PORTA &= ~((1<< PA0) | (1<<PA2));
-      }
+					new_message_received_flag=false;	// set the flag back to false
+			
+			if(leftStickV >= 600)//move forward
+			{
+				PORTA |= ((1<< PA0) | (1<<PA3));
+				PORTA &= ~((1<< PA1) | (1<<PA2));
+			}
+			else if(leftStickV <= 400) //move back
+			{
+				PORTA |= ((1<< PA1) | (1<<PA2));
+				PORTA &= ~((1<< PA0) | (1<<PA3));
+			}
+			else if(leftStickH >= 600) // move right
+			{
+				PORTA |= ((1<< PA0) | (1<<PA2));
+				PORTA &= ~((1<< PA1) | (1<<PA3));
+			}
+			else if(leftStickH <= 400)//move left
+			{
+				PORTA |= ((1<< PA1) | (1<<PA3));
+				PORTA &= ~((1<< PA0) | (1<<PA2));
+			}
+			else 
+			{
+				PORTA &= ~((1<< PA1) | (1<<PA3));
+				PORTA &= ~((1<< PA0) | (1<<PA2));
+			}
 		}
-
-
-    
-
-    
-  }
-  return(1);
+	}
+	return(1);
 }//end main 
 
 ISR(USART2_RX_vect)  // ISR executed whenever a new byte is available in the serial buffer
